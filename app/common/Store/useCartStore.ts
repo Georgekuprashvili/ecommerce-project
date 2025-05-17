f12be86d17f33056169862a55ca1fb68bcca6335
Georgekuprashvili <kuprashviligeorge@gmail.com>
2025-05-17 15:45:59 +0400

    feat: make cart drawer

---
+++ app/common/Store/useCartStore.ts
@@ -0,0 +1,43 @@
+import { create } from "zustand";
+
+export interface CartItem {
+  id: number;
+  name: string;
+  price: number;
+  quantity: number;
+  image: any;
+}
+
+interface CartStore {
+  items: CartItem[];
+  addItem: (item: CartItem) => void;
+  removeItem: (id: number) => void;
+  updateQuantity: (id: number, quantity: number) => void;
+  clearCart: () => void;
+  total: () => number;
+  itemCount: () => number;
+}
+
+export const useCartStore = create<CartStore>((set, get) => ({
+  items: [],
+  addItem: (item) => {
+    const existing = get().items.find((i) => i.id === item.id);
+    if (existing) {
+      set({
+        items: get().items.map((i) =>
+          i.id === item.id ? { ...i, quantity: i.quantity + item.quantity } : i
+        ),
+      });
+    } else {
+      set({ items: [...get().items, item] });
+    }
+  },
+  removeItem: (id) => set({ items: get().items.filter((i) => i.id !== id) }),
+  updateQuantity: (id, quantity) =>
+    set({
+      items: get().items.map((i) => (i.id === id ? { ...i, quantity } : i)),
+    }),
+  clearCart: () => set({ items: [] }),
+  total: () => get().items.reduce((acc, i) => acc + i.price * i.quantity, 0),
+  itemCount: () => get().items.reduce((acc, i) => acc + i.quantity, 0),
+}));
