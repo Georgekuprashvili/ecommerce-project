10865103bfc42de36750a0dc252f9dba021e1730
Georgekuprashvili <kuprashviligeorge@gmail.com>
2025-06-04 21:23:28 +0400

    first commit

--- app/common/api.ts
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-
-const token =
-  typeof window !== "undefined" ? localStorage.getItem("token") : "";
-
-export const api = axios.create({
-  baseURL: "http://localhost:4000",
-  headers: token ? { Authorization: `Bearer ${token}` } : {},
-});
